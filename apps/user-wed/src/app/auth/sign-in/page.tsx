f885457d52c2aaad98d1bb1514e384bad28e2c15
kiet.lt <kiet.lt@intelin.vn>
2025-02-23 17:30:42 +0700

    delete babel config

--- apps/user-wed/src/app/auth/sign-in/page.tsx
+++ apps/user-wed/src/app/auth/sign-in/page.tsx
@@ -1,23 +1,18 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @nx/enforce-module-boundaries */
-"use client";
 import type { Metadata } from "next";
 import Login from "userSrc/modules/auth/SignIn";
 import AuthLayout from "../../../layouts/AuthLayout";
-import ErrorBoundary from "./aaa";
 
-// export const metadata: Metadata = {
-//   title: "Sign In page",
-//   description: "Sign In page",
-// };
+export const metadata: Metadata = {
+  title: "Sign In page",
+  description: "Sign In page",
+};
 
 function LogInPage() {
   return (
-    <ErrorBoundary>
-      <AuthLayout>
-        <Login />
-      </AuthLayout>
-    </ErrorBoundary>
+    <AuthLayout>
+      <Login />
+    </AuthLayout>
   );
 }
 
