245efa4cafc1291d2eab6a4511809d40ab3a9f34
kiet.lt <kiet.lt@intelin.vn>
2025-02-23 16:53:23 +0700

    fix build

--- apps/user-wed/src/app/auth/sign-in/aaa.tsx
+++ apps/user-wed/src/app/auth/sign-in/aaa.tsx
@@ -4,29 +4,43 @@ import { Component } from 'react';
 
 class ErrorBoundary extends Component {
   constructor(props) {
-    super(props);
-    this.state = { hasError: false };
+    super(props)
+ 
+    // Define a state variable to track whether is an error or not
+    this.state = { hasError: false, error: null }
   }
-
   static getDerivedStateFromError(error) {
-    // Update state so the next render shows the fallback UI
-    return { hasError: true };
+    // Update state so the next render will show the fallback UI
+ 
+    return { hasError: true, error }
   }
-
   componentDidCatch(error, errorInfo) {
-    // Log the error to an error reporting service console
-    console.error('Error caught by Error Boundary:', error, errorInfo);
+    // You can use your own error logging service here
+    console.log({ error, errorInfo })
   }
-
   render() {
+    // Check if the error is thrown
     // @ts-ignore
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          {/* @ts-ignore */}
+          <pre>{this.state.error && this.state.error.toString()}</pre>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      )
     }
-
+ 
+    // Return children components in case of no error
     // @ts-ignore
-    return this.props.children;
+    return this.props.children
   }
 }
 
